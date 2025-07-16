72de6838fdd0d089fd5bfc69a4faf6f37a2ccc72
gilricardo-com <contact@gilricardo.com>
2025-07-16 19:59:33 -0400

    update document js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,26 +5,14 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Google Fonts can also be moved here from _app.js or Layout.js if they are static */}
-          <link 
-            rel="stylesheet" 
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
-            integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" 
-            crossOrigin="anonymous" 
-            referrerPolicy="no-referrer" 
-          />
           {/* Leaflet CSS */}
           <link 
             rel="stylesheet" 
             href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-            crossOrigin="anonymous"
           />
           {/* Leaflet JS */}
           <script 
-            src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" 
-            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" 
-            crossOrigin="anonymous"
+            src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
           ></script>
           {/* Add any other global font links or meta tags here */}
         </Head>
